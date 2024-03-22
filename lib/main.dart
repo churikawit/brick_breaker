afe43b48b8860feedd273de5a83be34ac2fabc85
Karn Churikawit <churikawit@hotmail.com>
2024-03-22 15:32:12 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'src/brick_breaker.dart';  
+
+void main() {
+  // final game = FlameGame();
+  final game = BrickBreaker();
+  runApp(GameWidget(game: game));
+}
